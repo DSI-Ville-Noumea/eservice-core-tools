53697e0012a5dc1a165092e40666d26231c09fed
Benfabre <60900133+Benfabre@users.noreply.github.com>
2020-11-26 15:30:43 +1100

    First commit
    Ajout KeycloakUserDto

---
+++ src/main/kotlin/nc/noumea/mairie/eservicecoretools/dto/KeycloakUserDto.kt
@@ -0,0 +1,11 @@
+package nc.noumea.mairie.eservicecoretools.dto
+
+class KeycloakUserDTO {
+    var id: String? = null
+
+    var email: String? = null
+
+    var givenName: String? = null
+
+    var name: String? = null
+}
---
+++ src/test/kotlin/nc/noumea/mairie/eservicecoretools/dto/HelloTest.kt
@@ -0,0 +1,8 @@
+package nc.noumea.mairie.eservicecoretools.dto
+
+import org.junit.Test
+import kotlin.test.assertEquals
+
+class HelloTest {
+
+}
