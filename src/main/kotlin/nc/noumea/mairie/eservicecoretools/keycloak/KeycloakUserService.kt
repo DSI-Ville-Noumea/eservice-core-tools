92161461f9d6c53d6fcfd5a00bbeac8e4bd08ca7
Fabien Seznec <fabien.seznec@agilesoft.nc>
2021-10-11 15:08:13 +1100

    Fix sur la récupération de l'utilisateur courrant.

--- src/main/kotlin/nc/noumea/mairie/eservicecoretools/keycloak/KeycloakUserService.kt
+++ src/main/kotlin/nc/noumea/mairie/eservicecoretools/keycloak/KeycloakUserService.kt
@@ -1,13 +1,18 @@
 package nc.noumea.mairie.eservicecoretools.keycloak
 
 import org.jboss.resteasy.client.jaxrs.ResteasyClientBuilder
+import org.keycloak.KeycloakPrincipal
 import org.keycloak.OAuth2Constants
 import org.keycloak.admin.client.KeycloakBuilder
 import org.keycloak.admin.client.resource.RealmResource
+import org.keycloak.representations.AccessToken
 import org.keycloak.representations.idm.UserRepresentation
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.core.env.Environment
+import org.springframework.security.core.context.SecurityContext
+import org.springframework.security.web.context.HttpSessionSecurityContextRepository
 import org.springframework.stereotype.Service
+import javax.servlet.http.HttpSession
 
 
 @Service
@@ -40,6 +45,17 @@ open class KeycloakUserService {
         Realm.INTERNE -> "${configuration.interneAuthServerUrl}/realms/${configuration.interneRealm}/account/?referrer=${configuration.resource}"
         Realm.EXTERNE -> "${configuration.externeAuthServerUrl}/realms/${configuration.externeRealm}/account/?referrer=${configuration.resource}"
     }.replace("//", "/") // fix potential trailing slash issue
+
+    fun getCurrentToken(httpSession: HttpSession): AccessToken? {
+        val securityContext = httpSession.getAttribute(HttpSessionSecurityContextRepository.SPRING_SECURITY_CONTEXT_KEY) as SecurityContext?
+        val keycloakPrincipal = securityContext?.authentication?.principal as KeycloakPrincipal<*>?
+        return keycloakPrincipal?.keycloakSecurityContext?.token
+    }
+
+    fun getCurrent(httpSession: HttpSession): UserRepresentation? = getCurrentToken(httpSession)?.let {
+        val realm = if (it.email?.endsWith("ville-noumea.nc") == true) Realm.INTERNE else Realm.EXTERNE
+        return findByUuid(realm, it.subject)
+    }
 
     private fun realmResource(realm: Realm) = when (realm) {
         Realm.INTERNE -> interneRealmResource
